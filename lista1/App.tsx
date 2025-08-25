8a4a89be8746cc84e9cc4c7384613deeb497d3d1
maucepinho <mauriciocepinho@gmail.com>
2025-08-24 23:03:50 -0300

    Exercício 9 modificação app.tsx

--- lista1/App.tsx
+++ lista1/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-//import Um from './screens/Um';
-//import Dois from './screens/Dois';
-//import Tres from './screens/Tres';
-//import Quatro from './screens/Quatro';
-//import Cinco from './screens/Cinco';
-//import Seis from './screens/Seis';
-//import Sete from './screens/Sete';
+import Um from './screens/Um';
+import Dois from './screens/Dois';
+import Tres from './screens/Tres';
+import Quatro from './screens/Quatro';
+import Cinco from './screens/Cinco';
+import Seis from './screens/Seis';
+import Sete from './screens/Sete';
 import Oito from './screens/Oito';
+import Nove from './screens/Nove';
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
     //<Cinco />
     //<Seis />
     //<Sete />
-    <Oito />
+    //<Oito />
+    <Nove />
   );
 }
