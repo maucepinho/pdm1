093b82d4f4ec98fe49dd2343c078ba496a700ed5
maucepinho <mauriciocepinho@gmail.com>
2025-08-24 22:25:02 -0300

    Exercício 3 modificação app.tsx

--- lista1/App.tsx
+++ lista1/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 //import Um from './screens/Um';
-import Dois from './screens/Dois';
+//import Dois from './screens/Dois';
+import Tres from './screens/Tres';
 
 export default function App() {
   return (
     //<Um />
-    <Dois />
+    //<Dois />
+    <Tres />
   );
 }
---
+++ lista1/screens/Tres.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function Tres() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <View style={styles.topLeftView} />
+        <View style={styles.topRightContainer}>
+          <View style={styles.topRightTopView} />
+          <View style={styles.topRightBottomView} />
+        </View>
+      </View>
+      <View style={styles.bottomView} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: Constants.statusBarHeight,
+  },
+  topContainer: {
+    flex: 0.5,
+    flexDirection: 'row',
+  },
+  topLeftView: {
+    flex: 0.5,
+    backgroundColor: 'lime',
+  },
+  topRightContainer: {
+    flex: 0.5,
+    flexDirection: 'column',
+  },
+  topRightTopView: {
+    flex: 0.5,
+    backgroundColor: 'teal', // Nova cor [cite: 49]
+  },
+  topRightBottomView: {
+    flex: 0.5,
+    backgroundColor: 'skyblue', // Nova cor [cite: 49]
+  },
+  bottomView: {
+    flex: 0.5,
+    backgroundColor: 'salmon',
+  },
+});
